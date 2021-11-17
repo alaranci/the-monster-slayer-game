9bdc341fe7e642f73e2fb3f4006945e97d0125e1
arayra <amandavianiva@outlook.com>
2021-11-18 05:07:55 +0700

    adding a special attack

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const app = Vue.createApp({
       name: "Irlan Navila",
       playerHealth: 100,
       monsterHealth: 100,
+      attackLaunch: 0,
     };
   },
 
   methods: {
     playerAttack() {
+      this.attackLaunch++;
       const fiery = Math.floor(Math.random() * (12 - 5)) + 5;
       this.monsterHealth -= fiery;
       this.monsterAttack();
@@ -16,6 +18,12 @@ const app = Vue.createApp({
     monsterAttack() {
       const fiery = Math.floor(Math.random() * (15 - 8)) + 8;
       this.playerHealth -= fiery;
+    },
+    playerSpecialAttack() {
+      this.attackLaunch++;
+      const fiery = Math.floor(Math.random() * (25 - 10)) + 10;
+      this.monsterHealth -= fiery;
+      this.monsterAttack();
     },
     heal() {},
     surrender() {},
